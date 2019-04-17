fb214e2a50b600d2018548c8f318612acccb6a88
Izuchukwu Ogbodo <matthiasogbodo@gmail.com>
2019-04-17 17:08:15 +0100

    fixed some bugs

--- Users/User.test.js
+++ Users/User.test.js
@@ -1,33 +1,19 @@
-const adminUser = require("./Admin");
 const normalUser = require("./User");
 
-var admin, user;
-
-test("create new admin user", function() {
-  admin = new adminUser("Admin1", "solomon@gmail.com", "solomon");
-  expect(admin.name).toBe("Admin1");
-});
-
-test("Admin can read all users", function() {
-  expect(admin.getListOfUsers()).toBeTruthy();
-});
-
-test("Persist user profile detail into db.json file", function() {
-  var oldLength = admin.getListOfUsers().length;
-  var newAdmin = new adminUser("Admin2", "solomon@gmail.com", "king");
-  expect(newAdmin.getListOfUsers().length).toBe(oldLength + 1);
-});
+var user;
 
 test("create normal user", function() {
   user = new normalUser("User1", "solomon@gmail.com", "solomon1");
   expect(user.name).toBe("User1");
 });
-test("Admin can search for a user by id", function() {
-  expect(admin.getUserById(user.id).id).toBe(user.id);
-});
 
 test("User can search for a user by id", function() {
-  expect(user.getUserById(admin.id).id).toBe(admin.id);
+  expect(user.getUserById(user.id).id).toBe(user.id);
+});
+
+test("Persist user profile detail into db.json file", function() {
+  var newUser = new normalUser("User2", "solomon@gmail.com", "king");
+  expect(newUser.getUserById(newUser.id).id).toBe(newUser.id);
 });
 
 test("The case of none existing Id", function() {
@@ -35,13 +21,7 @@ test("The case of none existing Id", function() {
 });
 
 test("User cannot read all users", function() {
-  expect(user.getListOfUsers).toBeFalsy();
-});
-
-test("Admin can update the details of a user", function() {
-  expect(
-    admin.updateUserDetails(user.id, "Treasure", "tres@gmail.com", "tressy").id
-  ).toBe(user.id);
+  expect("getListOfUsers" in user).toBeFalsy();
 });
 
 test("User can update his/her details", function() {
@@ -54,27 +34,8 @@ test("User can update his/her details", function() {
   expect(user.id).toBe(user.id);
 });
 
-test("Admin can delete a user", function() {
-  var oldLength = admin.getListOfUsers().length;
-  var userToDelete = new normalUser(
-    "nameToDelete",
-    "solomon@gmail.com",
-    "king"
-  );
-
-  expect(admin.deleteUserById(userToDelete.id).length).toBe(oldLength);
-});
-
 test("User cannot delete a user", function() {
   expect("deleteUserById" in user).toBeFalsy();
-});
-
-test("Admin can search for a user by name", function() {
-  expect(admin.getUserByName(user.name)).toBeTruthy();
-});
-
-test("Should return false if no user by the name", function() {
-  expect(admin.getUserByName("Noname")).toBeFalsy();
 });
 
 test("User can search for a user by name", function() {
@@ -82,13 +43,6 @@ test("User can search for a user by name", function() {
   expect(user.getUserByName(user.name)).toBeTruthy();
 });
 
-test("Admin can delete all users", function() {
-  expect(admin.deleteAllUsers().length).toBe(0);
-});
 test("User cannot delete all users", function() {
   expect("deleteAllUsers" in user).toBeFalsy();
-});
-
-test("In the case of Admin deleting when db is empty", function() {
-  expect(admin.deleteUserById(user.id).length).toBeUndefined();
 });
